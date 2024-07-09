12e82f5ee6148d75c3739f3d46b5f3942b603933
강해 <82922283+kanghea@users.noreply.github.com>
2024-07-09 12:08:23 +0900

    text

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,14 +6,11 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
+
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
